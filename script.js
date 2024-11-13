2d258dd6a76311e0b244c51b3a85cc97db20b3c4
Sephydev <nicolas.bussiere@outlook.fr>
2024-11-13 23:09:22 +0100

    Add aria-label to button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ Array.prototype.forEach.call(questionHeader, (item) => {
     item.addEventListener("click", function(e) {
         const answers = document.getElementsByClassName("answer");
         const questionImg = document.getElementsByClassName("question-img");
+        const questionBtn = document.getElementsByClassName("question");
 
         let id = item.id;
         let isHidden = answers[id].classList.contains("hidden")
@@ -12,10 +13,12 @@ Array.prototype.forEach.call(questionHeader, (item) => {
             answers[id].classList.remove("hidden")
             questionImg[id].src = "./assets/img/icon-minus.svg"
             questionImg[id].alt = "A minus"
+            questionBtn[id].ariaLabel = "Show less"
         } else {
             answers[id].classList.add("hidden")
             questionImg[id].src = "./assets/img/icon-plus.svg"
             questionImg[id].alt = "A plus"
+            questionBtn[id].ariaLabel = "Show more"
         }
     })
 })
