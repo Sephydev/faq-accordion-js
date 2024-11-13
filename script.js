ced4ec4050409fd4588e477cbb9d163f3fbea71d
Sephydev <nicolas.bussiere@outlook.fr>
2024-11-13 20:05:08 +0100

    Add functionality & animation to button of first question

---
+++ script.js
@@ -0,0 +1,18 @@
+const btnQuestion = document.getElementById("question");
+let isHidden = false;
+
+btnQuestion.addEventListener("click", function(e) {
+    const answer = document.getElementById("answer");
+    const questionImg = document.getElementById("question-img");
+
+    if (isHidden) {
+        answer.classList.remove("hidden");
+        questionImg.src = "./assets/img/icon-plus.svg";
+        isHidden = false;
+    } else {
+        answer.classList.add("hidden");
+        questionImg.src = "./assets/img/icon-minus.svg";
+        isHidden = true;
+    }
+})
+    
